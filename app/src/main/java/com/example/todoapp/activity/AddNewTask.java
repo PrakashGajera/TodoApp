7449000f190bef32872f1f8ac5d2a21fcbad47e3
Prakash Gajera <prakashgajera24@gmail.com>
2023-06-14 23:41:46 +0530

    update code

--- app/src/main/java/com/example/todoapp/activity/AddNewTask.java
+++ app/src/main/java/com/example/todoapp/activity/AddNewTask.java
@@ -3,8 +3,10 @@ package com.example.todoapp.activity;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -45,34 +47,40 @@ public class AddNewTask extends AppCompatActivity {
             toDoModel = (ToDoModel) getIntent().getSerializableExtra("data");
             binding.edittext.setText(toDoModel.getTask());
             binding.etMessage.setText(toDoModel.getMessage());
-            binding.etIssueDate.setText(toDoModel.getTime());
+            binding.etTime.setText(toDoModel.getTime());
             binding.txtAMPM.setText(toDoModel.getAmpm());
             timeStamp = toDoModel.getTimestamp();
         }
 
         binding.imgBack.setOnClickListener(v -> onBackPressed());
         binding.txtCancel.setOnClickListener(v -> onBackPressed());
-        binding.etIssueDate.setOnClickListener(v12 -> openDatePicker(binding.etIssueDate));
-        binding.txtAMPM.setOnClickListener(v12 -> openDatePicker(binding.etIssueDate));
+        binding.etTime.setOnClickListener(v12 -> openDatePicker(binding.etTime));
+        binding.txtAMPM.setOnClickListener(v12 -> openDatePicker(binding.etTime));
 
         binding.txtAdd.setOnClickListener(v -> {
-            if (update) {
-                myDb.updateTask(toDoModel.getId(), binding.edittext.getText().toString(), binding.etMessage.getText().toString(), binding.etIssueDate.getText().toString(), binding.txtAMPM.getText().toString(), timeStamp);
+            if (TextUtils.isEmpty(binding.edittext.getText().toString())) {
+                Toast.makeText(this, "Please enter task", Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(binding.etTime.getText().toString())) {
+                Toast.makeText(this, "Please select time", Toast.LENGTH_SHORT).show();
             } else {
-                ToDoModel item = new ToDoModel();
-                item.setTask(binding.edittext.getText().toString());
-                item.setMessage(binding.etMessage.getText().toString());
-                item.setStatus(0);
-                item.setTime(binding.etIssueDate.getText().toString());
-                item.setAmpm(binding.txtAMPM.getText().toString());
-                item.setTimestamp(timeStamp);
-                myDb.insertTask(item);
-            }
+                if (update) {
+                    myDb.updateTask(toDoModel.getId(), binding.edittext.getText().toString(), binding.etMessage.getText().toString(), binding.etTime.getText().toString(), binding.txtAMPM.getText().toString(), timeStamp);
+                } else {
+                    ToDoModel item = new ToDoModel();
+                    item.setTask(binding.edittext.getText().toString());
+                    item.setMessage(binding.etMessage.getText().toString());
+                    item.setStatus(0);
+                    item.setTime(binding.etTime.getText().toString());
+                    item.setAmpm(binding.txtAMPM.getText().toString());
+                    item.setTimestamp(timeStamp);
+                    myDb.insertTask(item);
+                }
 
-            Intent intent = new Intent(AddNewTask.this, MainActivity.class);
-            setResult(900, intent);
-            startActivity(intent);
-            finish();
+                Intent intent = new Intent(AddNewTask.this, MainActivity.class);
+                setResult(900, intent);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 
